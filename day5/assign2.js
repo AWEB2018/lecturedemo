40d70410c5692731947e00a9f3ecdefc7270b53a
santosh <santosh.ece06@gmail.com>
2018-04-20 12:51:33 +0530

    day 5

---
+++ day5/assign2.js
@@ -0,0 +1,25 @@
+
+function likeCount(btnElemRef) {
+    try{
+        console.log(btnElemRef);
+        let firstChild = btnElemRef.children[0];
+
+        let likeCountStr = firstChild.innerHTML;
+        let likeCount = parseInt(likeCountStr);
+
+        likeCount += 1;
+
+        firstChild.innerHTML = " " + likeCount;
+
+    }catch(mukeshErr){
+        console.log(mukeshErr);
+    }
+}
+
+function dislikeCount() {
+    try{
+        console.log(btnElemRef);       
+    }catch(err){
+        console.log(err);
+    }
+}
