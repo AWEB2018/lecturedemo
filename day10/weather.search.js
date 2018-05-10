98696d1ba207bc3d5e64952e3448c97523ee6f5d
santosh <santosh.ece06@gmail.com>
2018-05-10 11:20:54 +0530

    day 11

--- day10/weather.search.js
+++ day10/weather.search.js
@@ -1,11 +1,17 @@
 function ajaxSearch() {
     try{
         let inputValue=$("#inputId").val();
+        inputValue = inputValue || "MUMBAI";
+        
+        $("#inputId").val(inputValue);
+
         let APPID = "7023923dd26a725da995c75b65864de5";
         
         let url = "http://api.openweathermap.org/data/2.5/forecast"
-        
         let inputData = {"q" : inputValue, "APPID": APPID, "units":"metric"};
+        
+        //let url = "http://api.openweathermap.org/data/2.5/forecast"
+        //url += "?q=mumbai&appid=7023923dd26a725da995c75b65864de5";
         let jqxhr = $.get(url, inputData);
 
         jqxhr.done(function(data){
@@ -13,12 +19,16 @@ function ajaxSearch() {
             paintPage(data);
         });
 
+        jqxhr.fail(function(xhr, status, err){
+            console.log(err);
+            alert("INVALID INPUT");
+        });
+
     }catch(err){
         console.log(err);
     }
 }
-function paintPage(data)
-{
+function paintPage(data){
     try{
         for(let i=0;i<data.list.length;i++)
         {
@@ -41,3 +51,10 @@ function paintPage(data)
         console.log(err);
     }
 }
+
+
+// DEFAULT CALL
+// ajaxSearch(); // PROBLEM :: DOCUMENT IS NOT READY
+$(document).ready(function(){
+   // ajaxSearch();
+});
